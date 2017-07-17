8da25a64ddd98cb5edec26905f9ad0c15eee59c0
mzb <331972146@qq.com>
2017-07-17 15:50:59 +0800

    归并排序

--- main.cpp
+++ main.cpp
@@ -1,51 +1,65 @@
 /*
- * 插入排序
- * 输入：n个数的一个序列
- * 输出：输入序列的一个排列
- *
- * 方法：原址排序，每次拿出一个数字，与该数字之前的每一个数字进行比较，如果小于前边的数字，就交换这两个数字
+ * 归并排序
+ * 输入：n个数字
+ * 输出：n个数字的有序序列
+ * 非原址排序
+ * 方法:把数组分成前后两部分，如果这两部分是排好序的，把这两部分合并成一个数组，合并过程中分别从两部分中依次选择较小的数字，则合并完成后的数组是严格排序的
+ * 递归的调用该过程，就能完成排序的过程
  */
+
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-void insert_sort(vector<int> &arr)
+void merge(vector<int> &arr,int beg1,int end1,int beg2,int end2)
 {
-    if(arr.empty())
-        return;
-    for(int i=1;i<arr.size();i++)
+    vector<int> larr(arr.begin()+beg1,arr.begin()+end1+1),
+                rarr(arr.begin()+beg2,arr.begin()+end2+1);
+    larr.push_back(INT32_MAX);
+    rarr.push_back(INT32_MAX);
+    int index1=0;
+    int index2=0;
+    for(int i=beg1;i<=end2;i++)
     {
-        int curr=i;
-        int index=i-1;
-        while(index>=0&&arr[curr]<arr[index])
+        if(larr[index1]>rarr[index2])
         {
-            int tmp=arr[curr];
-            arr[curr]=arr[index];
-            arr[index]=tmp;
-
-            curr=index;
-            index--;
+            arr[i]=rarr[index2];
+            index2++;
+        }
+        else
+        {
+            arr[i]=larr[index1];
+            index1++;
         }
     }
 }
 
-int main() {
-    std::cout << "插入排序" << std::endl;
-    vector<int> arr1{2,1,4,-2,4,5,12,55,12,45};
+void merge_sort(vector<int> &arr,int beg,int end)
+{
+    if(beg>=end)
+        return;
+    int mid=(beg+end)/2;
+    merge_sort(arr,beg,mid);
+    merge_sort(arr,mid+1,end);
+    merge(arr,beg,mid,mid+1,end);
+}
 
-    cout<<"arr1:"<<endl;
-    for(int n:arr1)
+int main()
+{
+    vector<int> arr{2,1,-2,0,12,44,54};
+    cout<<"arr:"<<endl;
+    for(int n:arr)
+    {
+        cout<<n<<' ';
+    }
+    cout<<endl<<"sorted arr:"<<endl;
+    merge_sort(arr,0,arr.size()-1);
+    for(int n:arr)
     {
         cout<<n<<' ';
     }
     cout<<endl;
-    insert_sort(arr1);
-    cout<<"sorted arr1:"<<endl;
-    for(int n:arr1)
-    {
-        cout<<n<<' ';
-    }
 
     return 0;
 }
