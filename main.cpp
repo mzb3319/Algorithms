eca0494117c99ad041e4155b42fe5bbdf46950e6
mzb <331972146@qq.com>
2017-07-17 17:05:32 +0800

    随机排列数组

--- main.cpp
+++ main.cpp
@@ -1,80 +1,57 @@
 /*
- * 最大子数组问题
- * 输入：n个数字的序列，有正有负
- * 输出：数组中子数组的和最大
+ * 随机排列一个数组
+ * 输入：n个数字的序列
+ * 输出：输出随机排列后的数组
  *
- * 方法1:采用分治策略，把数组分成两部分，最大子数组要么在分界点左边，要么在分界点右边，要么跨过分界点
- * 方法2:找规律，遍历一遍数组，遍历的过程中记录已经遍历过的数字的和，和一个最大值，如果和 大于0 则继续遍历，如果和大于已经记录的最大值，则用更新最大值，
- *       如果遍历过程中和 小于0，则说明之前遍历过的数字的和已经不适合加入到最终的结果中，则丢弃之前遍历的所有数字的和，即把和置为0，接着遍历并重复上边的过程
+ * 方法1:创建一个优先级数组，此优先级数组中的数字随机生成，然后根据此优先级数组对原数组进行排序
+ * 方法2:讲原数组中的每一个数字与随机位置的数字进行交换，此方法是原址的，选这个吧
+ *
  */
 
 #include <iostream>
 #include <vector>
+#include <random>
+#include <unordered_map>
+#include <cassert>
 
 using namespace std;
 
-int findCross(vector<int> &arr,int beg,int end)
+
+void random_sort(vector<int> &arr)
 {
-
-    int mid=(beg+end)/2;
-    int lMax=0,rMax=0;
-    int add=0;
-    for(int i=mid-1;i>=beg;i--)
-    {
-        add+=arr[i];
-        if(add>lMax)
-            lMax=add;
-    }
-    add=0;
-    for(int i=mid+1;i<=end;i++)
-    {
-        add+=arr[i];
-        if(add>rMax)
-            rMax=add;
-    }
-    int ret=arr[mid];
-    if(lMax>0)
-        ret+=lMax;
-    if(rMax>0)
-        ret+=rMax;
-    return ret;
-}
-
-int maxSubarr(vector<int> &arr,int beg,int end)
-{
-    int mid=(beg+end)/2;
-    int left=arr[beg];
-    if(beg<mid-1)
-        left=maxSubarr(arr,beg,mid-1);
-    int right=arr[end];
-    if((mid+1)<end)
-        right=maxSubarr(arr,mid+1,end);
-    int cross=findCross(arr,beg,end);
-
-    int ret=right>left?right:left;
-    return ret>cross?ret:cross;
-}
-
-int maxSubarr(vector<int> &arr)
-{
-    int add=0;
-    int max=INT32_MIN;
     for(int i=0;i<arr.size();i++)
     {
-        add+=arr[i];
-        if(add>max)
-            max=add;
-        if(add<0)
-            add=0;
+        int r=random()%arr.size();
+        int tmp=arr[i];
+        arr[i]=arr[r];
+        arr[r]=tmp;
     }
-    return max;
+}
+
+bool check(vector<int> &arr)
+{
+    unordered_map<int,int> table;
+    for(int n:arr)
+    {
+        table[n]++;
+    }
+    random_sort(arr);
+    for(int n:arr)
+    {
+        table[n]--;
+    }
+    for(auto i:table)
+    {
+        if(i.second!=0)
+            return false;
+    }
+    return true;
 }
 
 int main()
 {
     vector<int> arr{13,-3,-25,20,-3,-16,-23,18,20,-7,12,-5,-22,15,-4,7};
-    cout<<maxSubarr(arr,0,arr.size()-1)<<endl;
-    cout<<maxSubarr(arr)<<endl;
+    assert(check(arr));
 
     return 0;
 }
