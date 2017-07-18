8324fef45ff9d20ba4aeb62847de47d64dde1ffe
mzb <331972146@qq.com>
2017-07-18 15:03:31 +0800

    计数排序

--- main.cpp
+++ main.cpp
@@ -1,65 +1,46 @@
 /*
- * 快速排序
- * 方法：在数组中随机选一个元素，与最后一个元素进行交换（这样可以做到随机化），然后以最后一个元素的值 k 作为标准，遍历数组，遍历过程中存储一个额外的指针
- * r 用来指向最后一个比 k 小的元素，r 初始化为比数组首元素索引小 1 （beg-1），对每一个元素，如果 <=k ，则先把 r 增加1，再把 r 所在的值与当前元素的值交换
- * 是原址排序
+ * 计数排序
+ * 方法：假设数组中的元素都是在 0-k 之间，那么创建一个大小为 k 的数组 count 用来计数原数组中元素出现的个数。
+ * 非原址排序，需要额外的空间
  */
 #include "iostream"
 #include "vector"
 
 using namespace std;
 
-//交换数组中的两个元素
-void swap(vector<int> &arr,int a,int b)
+void counting_sort(vector<int> &arr,int k)
 {
-    if(a==b)
-        return;
-    arr[a]+=arr[b];
-    arr[b]=arr[a]-arr[b];
-    arr[a]=arr[a]-arr[b];
-}
-
-int partition(vector<int> &arr,int beg,int end)
-{
-    if(beg>=end)
-        return beg;
-
-    //用来做随机化处理
-    int rp=beg+rand()%(end-beg+1);
-    swap(arr,rp,end);
-
-    int r=beg-1;
-    for(int i=beg;i<=end;++i)
+    //计数数组
+    vector<int> count(k+1,0);
+    //存放排序后的数组
+    vector<int> tmpArr(arr.size());
+    //计数
+    for(int n:arr)
+        count[n]++;
+    //累加
+    for(int i=1;i<count.size();++i)
+        count[i]+=count[i-1];
+    //对数组进行排序,需要注意的是数组索引从0开始，这里每个元素的计数至少是 1
+    for(int i=arr.size()-1;i>=0;--i)
     {
-        if(arr[i]<=arr[end])
-        {
-            swap(arr,++r,i);
-        }
+        tmpArr[count[arr[i]]-1]=arr[i];
+        count[arr[i]]--;
     }
-    return r;
-}
-
-void quick_sort(vector<int> &arr,int beg,int end)
-{
-    if(beg>=end)
-        return;
-    int r=partition(arr,beg,end);
-    quick_sort(arr,beg,r-1);
-    quick_sort(arr,r+1,end);
+    for(int i=0;i<arr.size();++i)
+        arr[i]=tmpArr[i];
 }
 
 int main()
 {
-    vector<int> arr{2,1,4,9,3,5,1,3,5};
+    vector<int> arr{9,8,7,6,5,3,3,4,1,8,9,4,4,3,2,1,0};
     cout<<"arr:"<<endl;
     for(int n:arr)
         cout<<n<<' ';
     cout<<endl<<"sorted arr:"<<endl;
-    quick_sort(arr,0,arr.size()-1);
+    counting_sort(arr,9);
     for(int n:arr)
-    {
         cout<<n<<' ';
-    }
     cout<<endl;
+
     return 0;
 }
