cdacd7984f83103efcf4a1fd68e710984fa49a44
mzb <331972146@qq.com>
2017-07-18 16:08:07 +0800

    基数排序

--- main.cpp
+++ main.cpp
@@ -1,45 +1,62 @@
 /*
- * 计数排序
- * 方法：假设数组中的元素都是在 0-k 之间，那么创建一个大小为 k 的数组 count 用来计数原数组中元素出现的个数。
- * 非原址排序，需要额外的空间
+ * 基数排序
+ * 方法：把数字从最低位开始按位排序，基数排序需要基于一种稳定的排序方法
  */
 #include "iostream"
 #include "vector"
 
 using namespace std;
 
-void counting_sort(vector<int> &arr,int k)
+void counting_sort(vector<string> &arr,int k,int index)
 {
     //计数数组
     vector<int> count(k+1,0);
     //存放排序后的数组
-    vector<int> tmpArr(arr.size());
+    vector<string> tmpArr(arr.size());
     //计数
-    for(int n:arr)
-        count[n]++;
+    for(const string &str:arr)
+    {
+        char c='0';
+        if(index<str.size())
+            c=str[str.size()-index-1];
+        count[c-'0']++;
+    }
     //累加
     for(int i=1;i<count.size();++i)
         count[i]+=count[i-1];
     //对数组进行排序,需要注意的是数组索引从0开始，这里每个元素的计数至少是 1
     for(int i=arr.size()-1;i>=0;--i)
     {
-        tmpArr[count[arr[i]]-1]=arr[i];
-        count[arr[i]]--;
+        char c='0';
+        if(index<arr[i].size())
+            c=arr[i][arr[i].size()-index-1];
+        tmpArr[count[c-'0']-1]=arr[i];
+        count[c-'0']--;
     }
     for(int i=0;i<arr.size();++i)
         arr[i]=tmpArr[i];
 }
 
+void radix_sort(vector<string> &arr)
+{
+    int index=0;
+    for(const string &str:arr)
+        if(str.size()>index)
+            index=str.size();
+    for(int i=0;i<index;++i)
+        counting_sort(arr,9,i);
+}
+
 int main()
 {
-    vector<int> arr{9,8,7,6,5,3,3,4,1,8,9,4,4,3,2,1,0};
+    vector<string> arr{"1001","378","576","344","987"};
     cout<<"arr:"<<endl;
-    for(int n:arr)
-        cout<<n<<' ';
+    for(auto &str:arr)
+        cout<<str<<' ';
     cout<<endl<<"sorted arr:"<<endl;
-    counting_sort(arr,9);
-    for(int n:arr)
-        cout<<n<<' ';
+    radix_sort(arr);
+    for(auto &str:arr)
+        cout<<str<<' ';
     cout<<endl;
 
     return 0;
