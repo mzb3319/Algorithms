2f489d9e66958a8a2477ba3fb05791c9c6044cda
mzb <331972146@qq.com>
2017-07-31 16:30:41 +0800

    深度优先搜索（拓扑排序）

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 /*
- * 霍夫曼编码(变长编码)
- * 使用贪心算法，先文本中出现次数多的字符给较短的编码，出现次数少的字符用较长的编码，编码过程使用贪心算法
- *
+ * 深度优先搜索
+ * 深度优先搜索可以探查有向图中是否存在环，条件是产生后向边
+ * 深度优先搜索也可以产生有向无环图的拓扑排序，拓扑排序顺序与节点完成的时间相反
  */
 #include "iostream"
 #include "vector"
@@ -15,44 +15,60 @@
 
 using namespace std;
 
+/*
+ * -1 -> white
+ *  0 -> gray
+ *  1 -> black
+ */
 struct Node
 {
-    Node(int v):val(v),p(NULL),color(false),d(INT32_MAX){}
+    Node(int v):val(v),p(NULL),color(-1),d(INT32_MAX),f(INT32_MAX){}
     int val;
     Node *p;
-    bool color;
+    int color;
     int d;
+    int f;
 };
 
-void BFS(Node* node,unordered_map<Node*,unordered_set<Node*>> &m)
+void DFS_visit(unordered_map<Node*,unordered_set<Node*>> &m,Node *curr,int &time)
 {
-    if(node==NULL)
-        return;
-    deque<Node*> q{node};
-    node->color=true;
-    node->d=0;
-
-    int d=0;
-    while(!q.empty())
+    ++time;
+    curr->d=time;
+    curr->color=0;
+    for(auto it:m[curr])
     {
-        ++d;
-
-        Node *n=q.front();
-        q.pop_front();
-
-        for(Node *it:m[n])
+        if(it->color==-1)
         {
-            //已经探查过的节点直接跳过
-            if(it->color)
-                continue;
-            it->color=true;
-            it->p=n;
-            it->d=d;
-            q.push_back(it);
+            DFS_visit(m,it,time);
+        }
+        else if(it->color==0)
+        {
+            //有后向边，有向图中 产生环
+            cout<<"circle"<<endl;
+            cout<<curr->val<<"->"<<it->val<<endl;
+        }
+    }
+    curr->color=1;
+    curr->f=time;
+}
+
+void DFS(unordered_map<Node*,unordered_set<Node*>> &m)
+{
+    int time=0;
+    for(auto it:m)
+    {
+        if(it.first->color==-1)
+        {
+            DFS_visit(m,it.first,time);
+        }
+        else if(it.first->color==0)
+        {
+            //有后向边，有向图中产生环
+            cout<<"circle"<<endl;
+            cout<<it.first->val<<endl;
         }
     }
 }
-void printBFS(unordered_map<Node*,unordered_set<Node*>> &m,Node *a,Node *b);
 
 //从一个二维数组初始化一个图，二维数组中每一行的第一个元素代表一个节点，后边的元素代表与该节点相连的节点
 void init_BFS(vector<vector<int>> &l)
@@ -75,34 +91,21 @@ void init_BFS(vector<vector<int>> &l)
             m[table[i]].insert(table[l[i][j]-1]);
         }
     }
-    BFS(table[2],m);
-    printBFS(m,table[2],table[4]);
+    DFS(m);
 }
 
-//输出从一个节点a到节点b的最短路径，上边已经为每个节点的p属性赋值，p属性指向BFS树的父节点，所以只要递归的输出
-void printBFS(unordered_map<Node*,unordered_set<Node*>> &m,Node *a,Node *b)
-{
-    if(a==b)
-        cout<<b->val;
-    else if(b->p==NULL)
-        cout<<"no such path"<<endl;
-    else
-    {
-        printBFS(m,a,b->p);
-        cout<<"->"<<b->val;
-    }
-}
 
 int main()
 {
     /*
-     * 1|->2->5
-     * 2|->1->5->3->4
-     * 3|->2->4
-     * 4|->2->5->3
-     * 5|->4->1->2
+     * 1|->2->4
+     * 2|->5
+     * 3|->5
+     * 4|->2
+     * 5|->4
+     * 6|->6
      */
-    vector<vector<int>> l{{1,2,5},{2,1,5,3,4},{3,2,4},{4,2,5,3},{5,4,1,2}};
+    vector<vector<int>> l{{1,2,4},{2,5},{3,5},{4,2},{5,4},{6,6}};
     init_BFS(l);
     return 0;
 }
